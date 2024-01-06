1c2c57cdf9b807dcf991e28aa8ad1cc765112bb7
Abdelbaki Najjar <najjarabdelbaki21@gmail.com>
2024-01-06 12:39:12 +0100

    file updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.1"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "database___automation"
+PKG_NAME= "mongodb_connectA"
 AUTHOR_USER_NAME = "Najjar-abdelbaki"
 AUTHOR_EMAIL = "najjarabdelbaki21@gmail.com"
 
